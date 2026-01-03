c161a8214c11f90551290d569067581a5dc1f856
aayushmaanhooda <aayushmaan162@gmail.com>
2026-01-04 10:57:08 +1100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const headingStyle = { color: 'red' };
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={headingStyle}>Hello World, Welcome to aayushmaan BOT version 3</h1>
+    </div>
+  )
+}
+
+export default App
